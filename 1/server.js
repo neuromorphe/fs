040f29b4e055860c3cb9e0721cf732b36ad1c438
neymo <kewinextazzy@gmail.com>
2025-08-08 01:15:38 +0400

    Update server.js

--- 1/server.js
+++ 1/server.js
@@ -27,6 +27,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, '150.241.114.194', () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
